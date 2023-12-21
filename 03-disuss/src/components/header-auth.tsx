caae4c875e82d63249d4162161bbb0dcf9ae506a
tabrizihamid84 <tabrizihamid6367@gmail.com>
2023-12-21 13:37:02 +0330

    finish upto 74

---
+++ 03-disuss/src/components/header-auth.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import * as actions from "@/actions";
+import {
+  Avatar,
+  Button,
+  NavbarItem,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
+import { useSession } from "next-auth/react";
+
+export default function HeaderAuth() {
+  const session = useSession();
+
+  let authConetnt: React.ReactNode;
+
+  if (session.status === "loading") {
+    authConetnt = null;
+  } else if (session.data?.user) {
+    authConetnt = (
+      <Popover placement="left">
+        <PopoverTrigger>
+          <Avatar src={session.data.user.image || ""} />
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="p-4">
+            <form action={actions.signOut}>
+              <Button type="submit">Sign Out</Button>
+            </form>
+          </div>
+        </PopoverContent>
+      </Popover>
+    );
+  } else {
+    authConetnt = (
+      <>
+        <NavbarItem>
+          <form action={actions.signIn}>
+            <Button type="submit" color="secondary" variant="bordered">
+              Sign In
+            </Button>
+          </form>
+        </NavbarItem>
+        <NavbarItem>
+          <form action={actions.signIn}>
+            <Button type="submit" color="primary" variant="flat">
+              Sign Up
+            </Button>
+          </form>
+        </NavbarItem>
+      </>
+    );
+  }
+
+  return authConetnt;
+}
--- 03-disuss/src/components/header.tsx
+++ 03-disuss/src/components/header.tsx
@@ -1,60 +1,14 @@
-import * as actions from "@/actions";
-import { auth } from "@/auth";
 import {
-  Avatar,
-  Button,
   Input,
   Navbar,
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
 } from "@nextui-org/react";
 import Link from "next/link";
-import React from "react";
+import HeaderAuth from "./header-auth";
 
-export default async function Header() {
-  const session = await auth();
-
-  let authConetnt: React.ReactNode;
-  if (session?.user) {
-    authConetnt = (
-      <Popover placement="left">
-        <PopoverTrigger>
-          <Avatar src={session.user.image || ""} />
-        </PopoverTrigger>
-        <PopoverContent>
-          <div className="p-4">
-            <form action={actions.signOut}>
-              <Button type="submit">Sign Out</Button>
-            </form>
-          </div>
-        </PopoverContent>
-      </Popover>
-    );
-  } else {
-    authConetnt = (
-      <>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type="submit" color="secondary" variant="bordered">
-              Sign In
-            </Button>
-          </form>
-        </NavbarItem>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type="submit" color="primary" variant="flat">
-              Sign Up
-            </Button>
-          </form>
-        </NavbarItem>
-      </>
-    );
-  }
-
+export default function Header() {
   return (
     <Navbar className="shadow mb-6">
       <NavbarBrand>
@@ -67,7 +21,9 @@ export default async function Header() {
           <Input />
         </NavbarItem>
       </NavbarContent>
-      <NavbarContent justify="end">{authConetnt}</NavbarContent>
+      <NavbarContent justify="end">
+        <HeaderAuth />
+      </NavbarContent>
     </Navbar>
   );
 }
