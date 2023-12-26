0791a99bf8a45a6cc5da8c49f8a21e361a7b2f4e
tabrizihamid84 <tabrizihamid6367@gmail.com>
2023-12-26 16:33:49 +0330

    add post show

--- 03-disuss/src/components/topics/topic-create-form.tsx
+++ 03-disuss/src/components/topics/topic-create-form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Button,
   Input,
@@ -8,24 +10,45 @@ import {
 } from "@nextui-org/react";
 
 import * as actions from "@/actions";
+import FormButton from "@/components/common/form-button";
+import { useFormState } from "react-dom";
 
 const TopicCreateForm = () => {
+  const [formState, action] = useFormState(actions.createTopic, {
+    errors: {},
+  });
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={actions.createTopic}>
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
-            <Input label="Name" labelPlacement="outside" placeholder="Name" />
+            <Input
+              name="name"
+              label="Name"
+              labelPlacement="outside"
+              placeholder="Name"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(", ")}
+            />
             <Textarea
+              name="description"
               label="Description"
               labelPlacement="outside"
               placeholder="Describe your topic"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(", ")}
             />
-            <Button type="submit">Submit</Button>
+            {formState.errors._form ? (
+              <div className="p-2 bg-red-200 border border-red-400 rounded">
+                {formState.errors._form?.join(", ")}
+              </div>
+            ) : null}
+            <FormButton>Submit</FormButton>
           </div>
         </form>
       </PopoverContent>
---
+++ 03-disuss/src/db/queries/posts.ts
@@ -0,0 +1,21 @@
+import { db } from "@/db";
+import type { Post } from "@prisma/client";
+
+export type PostWithData = Post & {
+  topic: { slug: string };
+  user: { name: string | null };
+  _count: { comments: number };
+};
+
+// export type PostWithData = Awaited<ReturnType<typeof fetchPostsByTopicSlug>>[number]
+
+export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
+  return db.post.findMany({
+    where: { topic: { slug } },
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true } },
+      _count: { select: { comments: true } },
+    },
+  });
+}
